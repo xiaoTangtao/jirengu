1be5ae1a0ac2d5e2222e4f9831b4eafd691acf99
Ambler <13790020331@163.com>
2018-02-20 10:09:07 +0800

    修改目录5task为task5 5.0

---
+++ task5/demo.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+//得到一个fs对象
+let fs = require("fs")
+//得到输入的项目名
+var dirName = process.argv[2]
+//获取当前目录
+var currentDirName = process.cwd()
+var flag = fsExistsSync(currentDirName+"/"+dirName)
+if (!flag) {
+    //进入到当前目录
+    process.chdir(currentDirName)
+    //新建那几个必须的目录和文件
+    fs.mkdirSync("./" + dirName) // mkdir $1
+    process.chdir("./" + dirName) // cd $1
+    fs.mkdirSync('css') // mkdir css
+    fs.mkdirSync('js') // mkdir js
+    fs.writeFileSync("./index.html", "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>")
+    fs.writeFileSync("css/style.css", "h1{color: red;}")
+    fs.writeFileSync("./js/main.js", "var string = 'Hello World'\nalert(string)")
+} else {
+    console.log("Directory already exists")
+}
+
+//检测文件或者文件夹存在 nodeJS
+function fsExistsSync(path) {
+    try{
+        fs.accessSync(path,fs.F_OK);
+    }catch(e){
+        return false;
+    }
+    return true;
+}
